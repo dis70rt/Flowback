d8774a598335c88420d19c76b7500c2424b10a9f
Saikat Das <sd.saikat369@gmail.com>
2026-09-02 23:27:51 +0530

    feat: add CallMockup component and integrate into Workspace for phone call handling

--- apps/frontend/src/components/mockups/index.ts
+++ apps/frontend/src/components/mockups/index.ts
@@ -2,3 +2,4 @@ export { SmsMockup as SmsAppMockup } from './SmsMockup';
 export { WhatsAppMockup as WhatsAppAppMockup } from './WhatsAppMockup';
 export * from './utils';
 export { EmailMockup as EmailAppMockup } from './EmailMockup';
+export { CallMockup as CallAppMockup } from './CallMockup';
